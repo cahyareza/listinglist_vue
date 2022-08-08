41a2c5dc26826b2b12951886629ae8376edc3db7
cahyafaisalreza <cf.reza07@gmail.com>
2022-08-08 14:26:55 +0700

    App_option_api

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app" :class="{ 'has-background-black': isDark }">
+    <div class="container is-max-desktop py-6 px-4">
+      <div v-if="loading">
+        <progress class="progress is-small is-info" max="100">60%</progress>
+      </div>
+      <div v-if="!loading">
+        <ListingsList :listings="listings" :isDark="isDark" />
+      </div>
+      <button class="button is-small is-pulled-right my-4"
+        @click="toggleDarkMode">
+        {{ darkModeButtonText }}
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapGetters } from 'vuex';
+import ListingsList from './components/ListingsList';
+import store from './store';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      isDark: false,
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'listings',
+      'loading'
+    ]),
+    darkModeButtonText() {
+      return this.isDark ? 'Light Mode' : 'Dark Mode';
+    }
+  },
+  methods: {
+    toggleDarkMode() {
+      this.isDark = !this.isDark;
+    }
+  },
+  created() {
+    this.$store.dispatch('getListings');
+  },
   components: {
-    HelloWorld
+    ListingsList
   }
 }
 </script>
